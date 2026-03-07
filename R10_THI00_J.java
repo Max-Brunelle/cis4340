6870392e278850fddf08d32fbdce2b41c0eca471
Max-Brunelle <maxbrunelle06@gmail.com>
2026-03-06 23:11:02 -0500

    Add noncompliant R10_THI00_J

---
+++ R10_THI00_J.java
@@ -0,0 +1,11 @@
+public final class R10_THI00_J implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("Running task");
+    }
+
+    public static void main(String[] args) {
+        R10_THI00_J foo = new R10_THI00_J();
+        new Thread(foo).run();
+    }
+}
