30ae110864970420cc684857cdf57333896995fe
Max-Brunelle <maxbrunelle06@gmail.com>
2026-03-06 20:38:34 -0500

    Add noncompliant R00_IDS03_J

---
+++ R00_IDS03_J.java
@@ -0,0 +1,17 @@
+import java.util.logging.Logger;
+
+public class R00_IDS03_J {
+    private static final Logger logger = Logger.getLogger(R00_IDS03_J.class.getName());
+
+    public static void logLogin(boolean loginSuccessful, String username) {
+        if (loginSuccessful) {
+            logger.severe("User login succeeded for: " + username);
+        } else {
+            logger.severe("User login failed for: " + username);
+        }
+    }
+
+    public static void main(String[] args) {
+        logLogin(false, "bad_user<script>");
+    }
+}
