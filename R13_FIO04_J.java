c03d3d84b66dbb950aa3a87d2961a656d9812f27
Max-Brunelle <maxbrunelle06@gmail.com>
2026-03-07 12:47:40 -0500

    Fix R13_FIO04_J

--- R13_FIO04_J.java
+++ R13_FIO04_J.java
@@ -4,10 +4,7 @@ import java.io.IOException;
 
 public class R13_FIO04_J {
     public static void main(String[] args) {
-        BufferedReader reader = null;
-
-        try {
-            reader = new BufferedReader(new FileReader("test.txt"));
+        try (BufferedReader reader = new BufferedReader(new FileReader("test.txt"))) {
             System.out.println(reader.readLine());
         } catch (IOException e) {
             System.out.println("Error reading file");
