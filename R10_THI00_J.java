c797178355cd062aec9182132ecfdb9f6211422c
Max-Brunelle <maxbrunelle06@gmail.com>
2026-03-06 23:12:32 -0500

    Fix R10_THI00_J

--- R10_THI00_J.java
+++ R10_THI00_J.java
@@ -6,6 +6,6 @@ public final class R10_THI00_J implements Runnable {
 
     public static void main(String[] args) {
         R10_THI00_J foo = new R10_THI00_J();
-        new Thread(foo).run();
+        new Thread(foo).start();
     }
 }
