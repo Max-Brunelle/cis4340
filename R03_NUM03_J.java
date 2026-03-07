2f3390d56e12e89c575d569c4c695386a079b239
Max-Brunelle <maxbrunelle06@gmail.com>
2026-03-06 20:41:41 -0500

    Fix R03_NUM03_J

--- R03_NUM03_J.java
+++ R03_NUM03_J.java
@@ -2,11 +2,11 @@ import java.io.DataInputStream;
 import java.io.IOException;
 
 public class R03_NUM03_J {
-    public static int getInteger(DataInputStream is) throws IOException {
-        return is.readInt();
+    public static long getInteger(DataInputStream is) throws IOException {
+        return is.readInt() & 0xFFFFFFFFL;
     }
 
     public static void main(String[] args) {
-        System.out.println("R03_NUM03_J noncompliant example");
+        System.out.println("R03_NUM03_J compliant example");
     }
 }
