73d74f721d17e97fe53c194734a119ebd8f7a505
Max-Brunelle <maxbrunelle06@gmail.com>
2026-03-06 23:10:04 -0500

    Fix R05_EXP00_J

--- R05_EXP00_J.java
+++ R05_EXP00_J.java
@@ -1,7 +1,7 @@
 public class R05_EXP00_J {
     public static void main(String[] args) {
         String original = "insecure";
-        original.replace('i', '9');
+        original = original.replace('i', '9');
         System.out.println(original);
     }
 }
